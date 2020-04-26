4623e30749bc5df743c466ca4da04300302b961b
Natasa Lukic <natasalukic2@gmail.com>
2020-04-26 20:52:28 +0200

    added user profile and change password layouts

---
+++ FindACar/app/src/main/java/com/example/findacar/activites/UserProfileActivity.java
@@ -0,0 +1,35 @@
+package com.example.findacar.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.findacar.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+    private TextView name;
+    private TextView surname;
+    private TextView email;
+    private Button btnChangePassword;
+    private Button btnMyReservations;
+    private ImageButton editName;
+    private ImageButton editSurname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        name = findViewById(R.id.textViewName);
+        surname = findViewById(R.id.textViewSurname);
+        email = findViewById(R.id.textViewEmail);
+        btnChangePassword = (Button) findViewById(R.id.btnChangePass);
+        btnMyReservations = (Button) findViewById(R.id.btnMyReservations);
+        editName = (ImageButton) findViewById(R.id.btnEditName);
+        editSurname = (ImageButton) findViewById(R.id.btnEditSurname);
+
+    }
+}
